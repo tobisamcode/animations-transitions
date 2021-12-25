8350de3fb7abfb0c900a8b270cc8146413b19083
tobisamcode <samueloluwatobiloba48@gmail.com>
2021-12-25 23:16:12 +0100

    adding  multiples children in the transition

--- src/App.vue
+++ src/App.vue
@@ -12,12 +12,13 @@
   </div>
 
 
-  <transition name="fade-button">
-    <div class="container">
+  
+  <div class="container">
+    <transition name="fade-button" mode="out-in">
       <button @click="showUsers()" v-if="!usersAreVisible">Show Users</button>
-      <button @click="hideUsers()" v-if="usersAreVisible">Hide Users</button>
-    </div>
-  </transition>
+      <button @click="hideUsers()" v-else>Hide Users</button>
+    </transition>
+  </div>
   
   <base-modal @close="hideDialog" :open="dialogIsVisible">
     <p>This is a test dialog!</p>
@@ -141,6 +142,25 @@ button:active {
 .para-leave-to{
   opacity: 0;
   transform: translateY(30px);
+}
+
+
+.fade-button-enter-from,
+.fade-button-leave-to{
+  opacity: 0;
+}
+
+.fade-button-enter-active{
+  transition: opacity .3s ease-out;
+}
+
+.fade-button-leave-active{
+  transition: opacity .3s ease-in;
+}
+
+.fade-button-enter-to,
+.fade-button-leave-from{
+  opacity: 1;
 }
 
 
