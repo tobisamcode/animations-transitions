a9ceb9e00d76d141899d8be87bee7a48315254ff
tobisamcode <samueloluwatobiloba48@gmail.com>
2021-12-27 12:25:00 +0100

    adding user list components

---
+++ src/components/UsersList.vue
@@ -0,0 +1,15 @@
+<template>
+    <ul>
+        <li v-for="user in users" :key="user"> {{ user }} </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    data() {
+        return{
+            users:['Tobi', 'Banji', 'Subomi', 'Grace', 'Micheal', 'Margeret', 'Raman']
+        }
+    }
+}
+</script>
